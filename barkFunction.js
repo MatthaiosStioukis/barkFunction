9ef43337c4896c3395beba30a701c66260b1e9f6
MatthaiosStioukis <stioukimattigr@gmail.com>
2023-01-13 01:11:52 +0200

    create my first javascript function Bark function.

---
+++ barkFunction.js
@@ -0,0 +1,14 @@
+
+function bark(name, weight) {
+    if (weight > 20){
+        console.log(name + " say's WOOF WOFF");
+    }else {
+        console.log(name + " say's woof woof");
+    }
+}
+
+bark("Metse", 12);
+bark("Eva", 22);
+bark("Bully", 50);
+bark("Rouli", 15);
+bark("Batman", 7);
